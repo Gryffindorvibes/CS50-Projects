aa4b5d3765803e10582dc9f9443ff6f7174a25ed
Gryffindorvibes <136833870+Gryffindorvibes@users.noreply.github.com>
2024-08-05 13:47:41 -0600

    Add files via upload
    Files for the website

---
+++ logic.js
@@ -0,0 +1,9 @@
+var home = document.getElementById("dub");
+home.addEventListener('click', function(){
+    alert("Welcome To Reichenbach Falls")
+});
+
+
+function gotoLink() {
+    location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
